4a39d29ea1cabdf38df6078dcfe53a85d5ad6989
marceloSchneider <marcelo.fialho@iag.usp.br>
2015-08-27 10:47:58 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 
 echo "Iniciando deploy"
+
+curDir = pwd
+$curDir/tsuru service-add mysql marcelo-quinta-mysql 
+
+$curDir/tsuru app-create $1
+
+serviceName = "mysql-$1"
+$curDir/tsuru service-add mysql $serviceName
+
+echo "relacionando servico"
+
+$curDir/tsuru service-bind $serviceName -a $1
+
+echo "clonando"
